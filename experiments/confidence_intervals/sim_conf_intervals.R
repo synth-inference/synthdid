1972bdb1ecc9e4a66578bcf8f7952aad83fc21a2
David Abraham Hirshberg <davidahirshberg@gmail.com>
2019-07-02 05:06:14 -0700

    reverted to all-rows jackknife, with out without holding the weights constant, for variance estimation -- this improvse coverage. Also: fixed infeasible (negative) weights issue with OSQP solver, added option to use (slower) CVXR solvers instead, and renamed a few things.

--- experiments/confidence_intervals/sim_conf_intervals.R
+++ experiments/confidence_intervals/sim_conf_intervals.R
@@ -1,42 +1,42 @@
 library(mvtnorm)
 library(synthdid)
 
-n_0 <- 100
-n_1 <- 10 
+N_0 <- 100
+N_1 <- 20 
 T_0 <- 120
-T_1 <- 20
-n <- n_0 + n_1
+T_1 <- 5
+N <- N_0 + N_1
 T <- T_0 + T_1
 tau <- 1
 sigma <- 2
 rank <- 2
 rho <- 0.7
 var <- outer(1:T, 1:T, FUN=function(x, y) rho^(abs(x-y)))
-W <- (1:n > n_0) %*% t(1:T > T_0)
+W <- (1:N > N_0) %*% t(1:T > T_0)
 
-mu.reps <- 100
-noise.reps <- 25
-include.slow=FALSE
+mu.reps <- 10
+noise.reps <- 10
+include.slow=TRUE
 
 results_full <- matrix(0, ncol = 3, nrow = mu.reps*noise.reps)
    
 for (l in 1:mu.reps){
-    print(l)
-    U <- matrix(rpois(rank * n, sqrt(1:n) / sqrt(n)), n, rank)
+    U <- matrix(rpois(rank * N, sqrt(1:N) / sqrt(N)), N, rank)
     V <- matrix(rpois(rank * T, sqrt(1:T) / sqrt(T)), T, rank)
     mu <- U %*% t(V)
     
     for (e in 1:noise.reps){
-        error <- rmvnorm(n, sigma = var, method = "chol")
+        print(c(l,e))
+        error <- rmvnorm(N, sigma = var, method = "chol")
         Y <- mu + tau * W  + sigma * error 
 
-        tau.sdid = sdid_est(Y,n_0,T_0)
+        tau.sdid = synthdid_estimate(Y,N_0,T_0)
         t.sdid = (tau.sdid - tau)/attr(tau.sdid,'se')
-        tau.did = did_est(Y,n_0,T_0)
+        tau.did = did_estimate(Y,N_0,T_0)
         t.did = (tau.did - tau)/attr(tau.did,'se')
         
         if(include.slow) {
-            tau.sdid.slow = sdid_est(Y,n_0,T_0, fast.var=FALSE)
+            tau.sdid.slow = synthdid_estimate(Y,N_0,T_0,fast.var=FALSE)
             t.sdid.slow = (tau.sdid.slow - tau)/attr(tau.sdid.slow,'se')
         } else { 
             t.sdid.slow = NA 
@@ -47,7 +47,11 @@ for (l in 1:mu.reps){
     }
 }
 
-
+    
+print(paste('coverage', 'synthdid',           round(mean(abs(results_full[,3]) < 1.96),4),
+                        'synthdid-fastvar',   round(mean(abs(results_full[,1]) < 1.96),4),
+                        'did',                round(mean(abs(results_full[,2]) < 1.96),4)))
+    
 
 pdf(sprintf('qq_sdid_cor_%d.pdf', 100*rho), width=5,height=5,paper='special') 
 qqnorm(results_full[,1], pch = 1, frame = F, cex = 0.5, main = NULL)
@@ -66,16 +70,6 @@ qqnorm(results_full[,3], pch = 1, frame = F,cex = 0.5,main = NULL)
 qqline(rnorm(mu.reps*noise.reps), col = "black", lwd = 1, lty = 2)
 box(lwd=0.1)        
 dev.off()
-
-    
-print(paste('coverage', 'sdid',      round(mean(abs(results_full[,1]) < 1.96),4),
-                        'sdid-slow', round(mean(abs(results_full[,3]) < 1.96),4),
-                        'did',       round(mean(abs(results_full[,2]) < 1.96),4)))
-    
-
-    
-   
-    
     
     
   
