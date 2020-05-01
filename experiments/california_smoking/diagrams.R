ed6e0427e97680d8f7d4c8183ee618c7c8114c9a
David Abraham Hirshberg <davidahirshberg@gmail.com>
2020-05-01 04:35:54 -0700

    updated plotting code to do a better job of overlaying multiple estimates as in diagrams.R.  And to plot SC as SC and not as SDID with no time weights. New stuff isn't documented. Somehow this breaks plotting a single estimate, so that'll need to be fixed before this gets merged into anything people use.

--- experiments/california_smoking/diagrams.R
+++ experiments/california_smoking/diagrams.R
@@ -1,59 +1,56 @@
+source('setup.R')
 source('../../R/synthdid.R')
-data.raw = read.table("MLAB_data.txt")
+library(viridis)
+sdid = synthdid_estimate(Y, N0, T0)
+sc = sc_estimate(Y, N0, T0)
+did = did_estimate(Y, N0, T0)
+sdid.i = function(i) { est = sdid; attr(est,'intercept') = i; est }
 
-STATE.NAME = c("Alabama", "Arkansas", "Colorado", "Connecticut", "Delaware",
-               "Georgia", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
-               "Kentucky", "Louisiana", "Maine", "Minnesota", "Mississippi",
-               "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
-               "New Mexico", "North Carolina", "North Dakota", "Ohio", "Oklahoma",
-               "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
-               "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "West Virginia",
-               "Wisconsin", "Wyoming", "California")
 
-STATE = data.raw[1,]
-X.attr = t(data.raw[2:8,])
-Y = t(data.raw[9:39,])
-colnames(Y) = 1969 + 1:31 
-rownames(Y) = STATE.NAME
-calif = which(STATE.NAME == "California")
-Y = Y[c(setdiff(1:nrow(Y), calif), calif), ]
-T0 = 19
-N0 = nrow(Y)-1
+synthdid_plot(list(sdid=sdid.i(0), sc=sc, junk=sdid), facet=c(1,1,1), lambda.comparable=TRUE, 
+    trajectory.linetype = 1, trajectory.alpha=.5, effect.alpha=.5, diagram.alpha=1, alpha.multiplier=c(1,.1,0), effect.curvature=-.4) + theme(legend.position='off') + scale_color_viridis_d()
+ggsave('figures/smoking-parallel-diagram.pdf', width=7, height=4)
+synthdid_plot(list(sdid=sdid.i(.75), sc=sc, junk=sdid), facet=c(1,1,1), lambda.comparable=TRUE, 
+    trajectory.linetype = 1, trajectory.alpha=.5, effect.alpha=.5, diagram.alpha=1, alpha.multiplier=c(1,.1,0), effect.curvature=-.4) + theme(legend.position='off') + scale_color_viridis_d()
+ggsave('figures/smoking-parallel-close.pdf', width=7, height=4)
+synthdid_plot(list(sdid=sdid.i(1), sc=sc, junk=sdid), facet=c(1,1,1), lambda.comparable=TRUE, 
+    trajectory.linetype = 1, trajectory.alpha=.5, effect.alpha=.5, diagram.alpha=1, alpha.multiplier=c(1,.1,0), effect.curvature=-.4) + theme(legend.position='off') + scale_color_viridis_d()
+ggsave('figures/smoking-overlay-diagram.pdf', width=7, height=4)
+synthdid_plot(list(sdid=sdid.i(1), sc=sc, junk=sdid), facet=c(1,1,1), lambda.comparable=TRUE, 
+    trajectory.linetype = 1, trajectory.alpha=.5, effect.alpha=.5, diagram.alpha=1, alpha.multiplier=c(1,1,0), effect.curvature=-.4) + theme(legend.position='off') + scale_color_viridis_d()
+ggsave('figures/smoking-overlay-vs-sc.pdf', width=7, height=4)
 
-X.noise = rnorm(50*length(Y)); dim(X.noise)=c(dim(Y), 50)
-beta.noise = rnorm(50)
-estimates = list(synthdid_estimate(Y, N0, T0),
-                 synthdid_estimate(Y+contract3(X.noise[,,1:2], 5*beta.noise[1:2]), N0, T0, X.noise[,,1]))
-names(estimates) = c('no covariates', 'ten noise covariates')
 
-synthdid_rmse_plot(estimates)
-synthdid_plot(estimates[[3]], sc.plot=TRUE)
+estimate = sdid
+setup = attr(estimate, 'setup')
+weights = attr(estimate, 'weights')
+Y = setup$Y - contract3(setup$X, weights$beta)
+N0 = setup$N0; N1 = nrow(Y)-N0
+T0 = setup$T0; T1 = ncol(Y)-T0
+    
+lambda.did   = c(rep(1/T0, T0),  rep(0,T1))
+lambda.synth = c(weights$lambda, rep(0, T1)) 
+lambda.target = c(rep(0,T0), rep(1/T1, T1))
+omega.synth  = c(weights$omega,  rep(0, N1))
+omega.target = c(rep(0,N0),  rep(1/N1, N1))
+
+points = data.frame(y=c(Y %*% lambda.target, Y %*% lambda.synth, Y %*% lambda.did),
+                    state=rep(factor(rownames(Y)),3),
+		    ypost = rep(Y %*% lambda.target, 3),
+                    color=rep(factor(c('post', 'synth', 'did')), each=nrow(Y)))
+ggplot(points) + geom_point(aes(x=state,y=y,color=color)) + theme(axis.text.x = element_text(angle = 90, hjust = 1));        ggsave('figures/time-parallel.pdf')
+ggplot(points) + geom_point(aes(x=state,y=y-ypost,color=color)) + theme(axis.text.x = element_text(angle = 90, hjust = 1));  ggsave('figures/time-parallel-relative.pdf')
+ggplot(points[points$state != 'California', ]) + geom_boxplot(aes(x=color, y=y-ypost));                                      ggsave('figures/time-parallel-boxplot.pdf')
+
+Y = Y[omega.synth+omega.target > .02, ]
+points = data.frame(y=c(Y %*% lambda.target, Y %*% lambda.synth, Y %*% lambda.did),
+                    state=rep(factor(rownames(Y)),3),
+		    ypost = rep(Y %*% lambda.target, 3),
+                    color=rep(factor(c('post', 'synth', 'did')), each=nrow(Y)))
+ggplot(points) + geom_point(aes(x=state,y=y,color=color)) + theme(axis.text.x = element_text(angle = 90, hjust = 1));        ggsave('figures/sc-time-parallel.pdf')
+ggplot(points) + geom_point(aes(x=state,y=y-ypost,color=color)) + theme(axis.text.x = element_text(angle = 90, hjust = 1));  ggsave('figures/sc-time-parallel-relative.pdf')
+ggplot(points[points$state != 'California', ]) + geom_boxplot(aes(x=color, y=y-ypost));                                      ggsave('figures/sc-time-parallel-boxplot.pdf')
+
+
 source('../../R/synthdid.R')
-
-indices=which(rownames(Y) %in% c('Alabama', 'Arkansas', 'Mississippi', 'Louisiana', 'Georgia', 'Kentucky', 'Tennesee','Florida'))
-estimates = list(synthdid_estimate(Y[c(indices, nrow(Y)),], length(indices), T0), synthdid_estimate(Y, N0, T0), synthdid_estimate(Y, N0, T0, omega.intercept=FALSE))
-attr(estimates[[3]], 'weights')$lambda = rep(1/T0,T0)
-names(estimates) = c('southeast control states', 'all control states', 'sc')
-synthdid_plot(estimates, facet.vertical=FALSE)
-synthdid_plot(estimates[[1]], treated.name='california', control.name='synth. california') + theme(legend.position=c(.9,.9)
-
-
-Y.offset = Y[,6:ncol(Y)]
-Y.offset[1:N0,] = Y[1:N0, 1:(ncol(Y)-5)]
-estimates = list(synthdid_estimate(Y.offset, N0, T0-5), synthdid_estimate(Y, N0, T0))
-synthdid_plot(estimates)
-
-Y.offset = Y[,6:ncol(Y)]
-Y.offset[1:N0,] = Y[1:N0, 1:(ncol(Y)-5)]
-estimates = synthdid_estimate(Y.offset, N0, T0-5)
-plot(estimates)
-
-summary(estimates[[1]])
-synthdid_placebo_plot(estimates[[1]])
-synthdid_placebo_plot(estimates[[1]])
-synthdid_placebo_plot(estimates[[1]], overlay=TRUE)
-
-synthdid = estimates[[1]]
-did = synthdid_estimate(Y, N0, T0, weights=list(lambda=rep(1/T0,T0), omega=rep(1/N0,N0)))
-synthdid_plot(list(synthdid=synthdid, did=did)) 
-synthdid_plot(list(synthdid=synthdid, did=did), lambda.comparable=TRUE) + facet_grid(. ~ facet)
+synthdid_time_plot(sdid)
---
+++ experiments/california_smoking/setup.R
@@ -0,0 +1,20 @@
+data.raw = read.table("MLAB_data.txt")
+
+STATE.NAME = c("Alabama", "Arkansas", "Colorado", "Connecticut", "Delaware",
+               "Georgia", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
+               "Kentucky", "Louisiana", "Maine", "Minnesota", "Mississippi",
+               "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
+               "New Mexico", "North Carolina", "North Dakota", "Ohio", "Oklahoma",
+               "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
+               "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "West Virginia",
+               "Wisconsin", "Wyoming", "California")
+
+STATE = data.raw[1,]
+X.attr = t(data.raw[2:8,])
+Y = t(data.raw[9:39,])
+colnames(Y) = 1969 + 1:31 
+rownames(Y) = STATE.NAME
+calif = which(STATE.NAME == "California")
+Y = Y[c(setdiff(1:nrow(Y), calif), calif), ]
+T0 = 19
+N0 = nrow(Y)-1
