79525211d639fccd74b008ee1a85ff0ad7c40357
Erik Sverdrup <erik.sverdrup@gmail.com>
2020-05-14 20:24:14 -0700

    Add @import ggplot2

--- R/synthdid.R
+++ R/synthdid.R
@@ -325,6 +325,7 @@ synthdid_se = function(estimate, weights = attr(estimate, 'weights')) {
 #'        one facet but highlighting one or several. All plot elements associated with the estimate are displayed
 #'        with alpha multiplied by the corresponding element of alpha.multiplier. Defaults to a vector of ones.
 #' @export synthdid_plot
+#' @import ggplot2
 synthdid_plot = function(estimates, treated.name='treated', control.name='synthetic control', force.sc=FALSE, 
 			 facet=NULL, facet.vertical=TRUE, lambda.comparable = !is.null(facet), overlay=0, 
 			 lambda.plot.scale=3, trajectory.linetype=1, effect.curvature = 0,
