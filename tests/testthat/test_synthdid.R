88d18677fa76f97c3cf102b9f28cdbb11b54c458
Erik Sverdrup <erik.sverdrup@gmail.com>
2020-11-25 08:51:06 -0800

    rename `c` to something easier to read

--- tests/testthat/test_synthdid.R
+++ tests/testthat/test_synthdid.R
@@ -82,37 +82,37 @@ test_that("invariances hold with default options", {
   # 3. Invariance to scaling (all)
   # Re-mapping Yit <- c * Yit for c > 0 doesn't change weights and scales tau by c
   # 3.1: c is very large
-  c = 10^6
+  const = 10^6
   for (estimator in estimators) {
     estimate = estimator(setup$Y,
                          setup$N0,
                          setup$T0)
     estimate.se = synthdid_se(estimate)
     weights = attr(estimate, "weights")
-    estimate.scaled = estimator(c * setup$Y,
+    estimate.scaled = estimator(const * setup$Y,
                                 setup$N0,
                                 setup$T0)
     estimate.se.scaled = synthdid_se(estimate.scaled)
     weights.scaled = attr(estimate.scaled, "weights")
-    expect_equal(c(c * estimate), c(estimate.scaled))
-    expect_equal(c * estimate.se, estimate.se.scaled)
+    expect_equal(c(const * estimate), c(estimate.scaled))
+    expect_equal(const * estimate.se, estimate.se.scaled)
     expect_equal(weights[c("lambda", "omega")], weights.scaled[c("lambda", "omega")])
   }
   # 3.2: c is very small
-  c = 10^-6
+  const = 10^-6
   for (estimator in estimators) {
     estimate = estimator(setup$Y,
                          setup$N0,
                          setup$T0)
     estimate.se = synthdid_se(estimate)
     weights = attr(estimate, "weights")
-    estimate.scaled = estimator(c * setup$Y,
+    estimate.scaled = estimator(const * setup$Y,
                                 setup$N0,
                                 setup$T0)
     estimate.se.scaled = synthdid_se(estimate.scaled)
     weights.scaled = attr(estimate.scaled, "weights")
-    expect_equal(c(c * estimate), c(estimate.scaled))
-    expect_equal(c * estimate.se, estimate.se.scaled)
+    expect_equal(c(const * estimate), c(estimate.scaled))
+    expect_equal(const * estimate.se, estimate.se.scaled)
     expect_equal(weights[c("lambda", "omega")], weights.scaled[c("lambda", "omega")])
   }
 })
