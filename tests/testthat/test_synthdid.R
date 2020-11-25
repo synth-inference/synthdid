508b89068c7227e1f6e5dc5677f13859ff70d185
Erik Sverdrup <erik.sverdrup@gmail.com>
2020-11-24 21:12:08 -0800

    add test

--- tests/testthat/test_synthdid.R
+++ tests/testthat/test_synthdid.R
@@ -40,3 +40,79 @@ test_that("default synthdid behavior has not changed", {
   # saveRDS(coef, "coef.expected.Rds")
   # saveRDS(weights, "weights.expected.Rds")
 })
+
+test_that("invariances hold with default options", {
+  # Test that three types of invariances hold, for details see
+  # https://github.com/synth-inference/synthdid/issues/38
+  estimators = list(sc_estimate, did_estimate, synthdid_estimate)
+  setup = random.low.rank()
+
+  # 1. Invariance to column fixed effect (all)
+  # Re-mapping Yit <- Yit + bt for arbitrary bt doesn't change anything
+  bt = 2 * matrix(1:ncol(setup$Y), nrow(setup$Y), ncol(setup$Y), byrow = T)
+  for (estimator in estimators) {
+    estimate = estimator(setup$Y,
+                         setup$N0,
+                         setup$T0)
+    estimate.se = synthdid_se(estimate)
+    estimate.col.scaled = estimator(setup$Y + bt,
+                                    setup$N0,
+                                    setup$T0)
+    estimate.se.col.scaled = synthdid_se(estimate.col.scaled)
+    expect_equal(c(estimate), c(estimate.col.scaled))
+    expect_equal(estimate.se, estimate.se.col.scaled)
+  }
+
+  # 2. Invariance to row fixed effect (exception: "SC")
+  # Re-mapping Yit <- Yit + ai for arbitrary ai doesn't change anything
+  at = 2.5 * matrix(1:nrow(setup$Y), nrow(setup$Y), ncol(setup$Y), byrow = FALSE)
+  for (estimator in estimators[-1]) {
+    estimate = estimator(setup$Y,
+                         setup$N0,
+                         setup$T0)
+    estimate.se = synthdid_se(estimate)
+    estimate.row.scaled = estimator(setup$Y + bt,
+                                    setup$N0,
+                                    setup$T0)
+    estimate.se.row.scaled = synthdid_se(estimate.row.scaled)
+    expect_equal(c(estimate), c(estimate.row.scaled))
+    expect_equal(estimate.se, estimate.se.row.scaled)
+  }
+
+  # 3. Invariance to scaling (all)
+  # Re-mapping Yit <- c * Yit for c > 0 doesn't change weights and scales tau by c
+  # 3.1: c is very large
+  c = 10^6
+  for (estimator in estimators[-1]) {
+    estimate = estimator(setup$Y,
+                         setup$N0,
+                         setup$T0)
+    estimate.se = synthdid_se(estimate)
+    weights = attr(estimate, "weights")
+    estimate.scaled = estimator(c * setup$Y,
+                                setup$N0,
+                                setup$T0)
+    estimate.se.scaled = synthdid_se(estimate.scaled)
+    weights.scaled = attr(estimate.scaled, "weights")
+    expect_equal(c(c * estimate), c(estimate.scaled))
+    expect_equal(c * estimate.se, estimate.se.scaled)
+    expect_equal(weights[c("lambda", "omega")], weights.scaled[c("lambda", "omega")])
+  }
+  # 3.2: c is very small
+  c = 10^-6
+  for (estimator in estimators[-1]) {
+    estimate = estimator(setup$Y,
+                         setup$N0,
+                         setup$T0)
+    estimate.se = synthdid_se(estimate)
+    weights = attr(estimate, "weights")
+    estimate.scaled = estimator(c * setup$Y,
+                                setup$N0,
+                                setup$T0)
+    estimate.se.scaled = synthdid_se(estimate.scaled)
+    weights.scaled = attr(estimate.scaled, "weights")
+    expect_equal(c(c * estimate), c(estimate.scaled))
+    expect_equal(c * estimate.se, estimate.se.scaled)
+    expect_equal(weights[c("lambda", "omega")], weights.scaled[c("lambda", "omega")])
+  }
+})
