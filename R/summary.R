31a6e30603379f118769f5d79c91d65ed767ea85
David Abraham Hirshberg <davidahirshberg@gmail.com>
2021-03-13 08:17:03 -0800

    improved synthdid_controls, summary, and format also introduced and used some convenient generic accessors change to synthdid_controls interface:   it now always returns a 2d (#controls x #estimators) matrix   if #estimators=1, as is typical, this is #controls  x 1 summary can be slow   it calculates standard errors using the vcov default: bootstrap   pass fast=TRUE to use jackknife instead

--- R/summary.R
+++ R/summary.R
@@ -1,36 +1,42 @@
-#' Outputs a table of important synthetic controls and their corresponding weights omega, sorted by weight.
+#' Outputs a table of important synthetic controls and their corresponding weights, sorted by weight.
 #' The table is truncated to exclude synthetic controls that do not matter for any estimate ---
 #' for each estimate, the truncated controls may have total weight no larger that 1-mass.
 #' @param estimates, a list of estimates output by synthdid_estimate. Or a single estimate.
 #' @param sort.by, the index of the estimate to sort by. Defaults to 1.
-#' @param digits,  the number of digits of weight to display. Defaults to 3.
 #' @param mass, which controls the length of the table. Defaults to 0.9.
+#' @param weight.type, 'omega' for units, 'lambda' for time periods
 #' @export synthdid_controls
-synthdid_controls = function(estimates, sort.by = 1, digits = 3, mass = .9) {
+synthdid_controls = function(estimates, sort.by = 1, mass = .9, weight.type = 'omega') {
   if (class(estimates) == 'synthdid_estimate') { estimates = list(estimates) }
   if (is.null(names(estimates))) { names(estimates) = sprintf('estimate %d', 1:length(estimates)) }
-
-  omegas = do.call(cbind, lapply(estimates, function(est) { attr(est, 'weights')$omega }))
-  if (is.null(dim(omegas))) { dim(omegas) = c(length(omegas), 1) }
+  if (!weight.type %in% c('omega', 'lambda')) { stop('weight.type must be "omega" or "lambda"') } 
+  weights = do.call(cbind, lapply(estimates, function(est) { attr(est, 'weights')[[weight.type]] }))
+  if (is.null(dim(weights))) { dim(weights) = c(length(weights), 1) }
 
   Y = attr(estimates[[1]], 'setup')$Y
-  o = rev(order(omegas[, sort.by]))
-  tab = round(omegas[o, , drop = FALSE], digits = digits)
-  rownames(tab) = rownames(Y)[o]
+  o = rev(order(weights[, sort.by]))
+  tab = weights[o, , drop = FALSE]
+  rownames(tab) = if(weight.type == 'omega') { rownames(Y)[o] } else { colnames(Y)[o] }
   colnames(tab) = names(estimates)
 
   # truncate table to retain a weight sum of at least mass for each unit
   tab.len = max(apply(tab, 2, function(col) { Position(function(x) { x >= mass }, cumsum(col)) }))
-  tab[1:tab.len, ]
+  tab[1:tab.len, , drop=FALSE]
 }
 
 #' Summarize a synthdid object
 #' @param object The object to summarize
+#' @param weight.digits The number of digits to use when displaying weights (omega)
 #' @param ... Additional arguments (currently ignored).
 #' @method summary synthdid_estimate
 #' @export
-summary.synthdid_estimate = function(object, ...) {
+summary.synthdid_estimate = function(object, weight.digits=3, fast=FALSE, ...) {
+  N0 = attr(object, 'setup')$N0
+  T0 = attr(object, 'setup')$T0
   list(estimate = c(object),
-    se = synthdid_se(object),
-    controls = synthdid_controls(object))
+    se = sqrt(if(fast) { vcov(object, method = 'jackknife') } else { vcov(object) }),
+    controls = round(synthdid_controls(object, weight.type='omega'),  digits=weight.digits),
+    periods  = round(synthdid_controls(object, weight.type='lambda'), digits=weight.digits),
+    dimensions = c( N1 = nrow(Y(object))-N0, N0 = N0, N0.effective = round(1 / sum(omega(object)^2),  weight.digits),
+		    T1 = ncol(Y(object))-T0, T0 = T0, T0.effective = round(1 / sum(lambda(object)^2), weight.digits)))
 }
