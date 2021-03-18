fa2c344cd60ff278972ad73e740e6ed143dad021
David Abraham Hirshberg <davidahirshberg@gmail.com>
2021-03-18 16:47:07 -0700

    added reference solver in CVXR and tests that our implementation agrees with it on california data. TODO: better stopping rule for optimizers, ideally one based on a tolerance for the convergence of the SDID estimate.

---
+++ R/reference-solver.R
@@ -0,0 +1,34 @@
+# return x minimizing ||Ax - b||^2 + zeta^2 n || x ||^2                   if intercept=FALSE
+#      | x minimizing min_x0 || + ||Ax + x0 - b||^2 + zeta^2 n || x ||^2  if intercept=TRUE
+# here n = length(b)
+simplex.least.squares =  function(A, b, zeta = 0, intercept = FALSE) {
+    x = CVXR::Variable(ncol(A))
+    constraints = list(sum(x) == 1, x >= 0)  
+    if(intercept) {
+	x0 = CVXR::Variable(1)
+	objective = sum((A %*% x + x0 - b)^2) + zeta^2 * length(b) * sum(x^2) 
+    } else {
+	objective = sum((A %*% x - b)^2) + zeta^2 * length(b) * sum(x^2) 
+    }
+    cvx.problem = CVXR::Problem(CVXR::Minimize(objective), constraints)
+    cvx.output = CVXR::solve(cvx.problem, solver = 'ECOS')
+    as.numeric(cvx.output$getValue(x))
+} 
+
+sigma.default = function(Y, N0, T0) { sd(apply(Y[1:N0,1:T0], 1, diff)) }
+epsilon = 1e-6
+synthdid.reference = function(Y, N0, T0, zeta.omega=sigma.default(Y, N0, T0)) {
+    N = nrow(Y); T=ncol(Y); N1 = N-N0; T1=T-T0;
+    lambda = simplex.least.squares(Y[1:N0, 1:T0],    rowMeans(Y[1:N0, (T0+1):T, drop=FALSE]), zeta=epsilon*zeta.omega, intercept=TRUE)
+    omega =  simplex.least.squares(t(Y[1:N0, 1:T0]), colMeans(Y[(N0+1):N, 1:T0, drop=FALSE]), zeta=zeta.omega,         intercept=TRUE)
+    estimate = t(c(-omega, rep(1/N1, N1))) %*% Y  %*% c(-lambda, rep(1/T1, T1))
+}
+sc.reference = function(Y, N0, T0, zeta.omega=sigma.default(Y, N0, T0)) {
+    N = nrow(Y); T=ncol(Y); N1 = N-N0; T1=T-T0;
+    omega =  simplex.least.squares(t(Y[1:N0, 1:T0]), colMeans(Y[(N0+1):N, 1:T0, drop=FALSE]), zeta=zeta.omega, intercept=FALSE)
+    estimate = t(c(-omega, rep(1 / N1, N1))) %*% Y  %*% c(-rep(0, T0), rep(1/T1, T1))
+}
+did.reference = function(Y, N0, T0) {
+    N = nrow(Y); T=ncol(Y); N1 = N-N0; T1=T-T0;
+    estimate = t(c(-rep(1/N0, N0), rep(1 / N1, N1))) %*% Y  %*% c(-rep(1/T0, T0), rep(1 / T1, T1))
+}    
---
+++ tests/testthat/test_against_reference.R
@@ -0,0 +1,29 @@
+tol=.03
+min.decrease = 1e-6
+max.iter = 1e5
+
+test_that("synthdid point estimate agrees with the reference implementation", {
+    if (!requireNamespace("CVXR", quietly = TRUE)) { stop("Package CVXR must be installed to run this test.") }
+    data(california_prop99)
+    setup = panel.matrices(california_prop99)
+    expect_equal(c(synthdid_estimate(setup$Y, setup$N0, setup$T0, min.decrease=min.decrease, max.iter=max.iter)),
+		 c(synthdid:::synthdid.reference(setup$Y, setup$N0, setup$T0)), tol=tol)
+})
+
+test_that("sc point estimate agrees with the reference implementation", {
+    if (!requireNamespace("CVXR", quietly = TRUE)) { stop("Package CVXR must be installed to run this test.") }
+    data(california_prop99)
+    setup = panel.matrices(california_prop99)
+    expect_equal(c(sc_estimate(setup$Y, setup$N0, setup$T0, min.decrease=min.decrease, max.iter=max.iter)),
+		 c(synthdid:::sc.reference(setup$Y, setup$N0, setup$T0)), tol=tol)
+})
+
+test_that("did point estimate agrees with the reference implementation", {
+    if (!requireNamespace("CVXR", quietly = TRUE)) { stop("Package CVXR must be installed to run this test.") }
+    data(california_prop99)
+    setup = panel.matrices(california_prop99)
+    expect_equal(c(did_estimate(setup$Y, setup$N0, setup$T0)),
+		 c(synthdid:::did.reference(setup$Y, setup$N0, setup$T0)), tol=tol)
+})
+
+
